7fad2fe1b54329fc06804fbf02efe15d187a4c78
Giorgi Tsintsadze <giorgi.tsintsadze@scnadiweb.com>
2023-08-20 12:48:26 +0400

    refactor

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,17 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return str.matches("[0-9]+");
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        int num = Integer.parseInt(str);
+        return num > 0;
     }
 }
