a375431cdf93dcb79c12f095e9e9c3ad15f3dc45
Giorgi Tsintsadze <giotsintsadze94@gmail.com>
2023-09-07 19:29:27 +0400

    code refactor

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,18 +2,15 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        int i = 0;
-        while (i < str.length() && str.charAt(i) == ' ') {
-            i++;
-        }
-        if (i == str.length()) {
+        if (str == null) {
             return false;
         }
-        for (int j = i; j < str.length(); j++) {
-            if (!Character.isDigit(str.charAt(j))) {
-                return false;
-            }
+
+        try {
+            double num = Double.parseDouble(str);
+            return num > 0;
+        } catch (NumberFormatException e) {
+            return false;
         }
-        return true;
     }
 }
